559af4ee39bce5f659a881aec40374de8cb7a999
PedroHenriqueDEV26 <112581681+PedroHenriqueDEV26@users.noreply.github.com>
2023-12-14 15:53:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,68 @@
-function startClassification(){
+function startClassification() {
 
     navigator.mediaDevices.getUserMedia({
         audio: true
     });
-    
+
     classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/gcOJKTk90/model.json", modelRead)
 }
 
 
-    function modelRead(){
-        classifier.classify(gotResult)
+function modelRead() {
+    classifier.classify(gotResult)
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error)
+    }
+    // console.log(results)
+
+    r = Math.floor(Math.random() * 255) + 1;
+
+    g = Math.floor(Math.random() * 255) + 1;
+
+    b = Math.floor(Math.random() * 255) + 1;
+
+    document.getElementById("result_label").innerHTML = "Posso ouvir - "+ results[0].label;
+
+    document.getElementById("result_label").style.color = "rgb("+r+", "+g+", "+b+")";
+
+    document.getElementById("result_confidence").innerHTML = "Precisão - "+ (results[0].confidence*100).toFixed(2) + "%";
+
+    document.getElementById("result_confidence").style.color = "rgb("+r+", "+g+", "+b+")";
+
+    img1 = document.getElementById("kakashi");
+    img2 = document.getElementById("sasuke");
+    img3 = document.getElementById("mC");
+    img4 = document.getElementById("allmight");
+
+    if(results[0].label == "Estalo"){
+        img1.src = "kakashi-fortnite.gif";
+        img2.src = "sasuke.png";
+        img3.src = "MasterChief.webp";
+        img4.src = "All_Might.webp";
     }
 
-    function gotResult(error,results){
-        if(error){
-            console.error(error)
-        }
-        console.log(results)
+    else if(results[0].label == "Palma"){
+        img1.src = "kakashi-removebg-preview.png";
+        img2.src = "sasukegif.gif";
+        img3.src = "MasterChief.webp";
+        img4.src = "All_Might.webp";
     }
+
+    else if(results[0].label == "Assovio"){
+        img1.src = "kakashi-removebg-preview.png";
+        img2.src = "sasuke.png";
+        img3.src = "MasterChief.webp";
+        img4.src = "all-might-fortnite.gif";
+    }
+
+    else{
+        img1.src = "kakashi-removebg-preview.png";
+        img2.src = "sasuke.png";
+        img3.src = "masterchief.gif";
+        img4.src = "All_Might.webp";
+    }
+
+}
